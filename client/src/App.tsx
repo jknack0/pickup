7fe8c72563d28186b521a0bcc529a596ff9332ae
jknack0 <jonathonknack@gmail.com>
2025-12-13 11:13:52 -0600

    stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,42 @@
 import { useState } from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { theme } from './theme';
+
+const queryClient = new QueryClient();
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div>
-        <h1>Vite + React</h1>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <div>
+                    <h1>Vite + React</h1>
+                  </div>
+                  <div className="card">
+                    <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                    <p>
+                      Edit <code>src/App.tsx</code> and save to test HMR
+                    </p>
+                  </div>
+                </>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
